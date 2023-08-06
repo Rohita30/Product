dbdd4f7b8239a38c31e3297aedd1ddb8634f2a75
Rohita30 <rohitap938@gmail.com>
2023-08-06 12:36:19 +0530

    feign added

--- src/main/java/com/product/MicroserviceProductApplication.java
+++ src/main/java/com/product/MicroserviceProductApplication.java
@@ -1,18 +1,20 @@
 package com.product;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableFeignClients
 public class MicroserviceProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceProductApplication.class, args);
 	}
+	
 
 }
